5c60c4cd97bf56af60cbf0ebd836d1878402972a
Ankit kumar <2020ugcs004@nitjsr.ac.in>
2025-04-04 18:39:14 +0530

    Added Red theme to the Home Screen

--- app/src/main/java/com/example/assignment_habit/ui/theme/Color.kt
+++ app/src/main/java/com/example/assignment_habit/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.assignment_habit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Primary reds
+val PrimaryRed = Color(0xFFE53935)
+val LightRed = Color(0xFFFF6F60)
+val DarkRed = Color(0xFFAB000D)
+
+// Supporting colors
+val Orange = Color(0xFFFF9800)
+val Cream = Color(0xFFFFF8E1)
+val Grey80 = Color(0xFFD1D1D1)
+val Grey40 = Color(0xFF6D6D6D)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
